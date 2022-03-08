69268334bed41cc6a654d7281243f17487362d31
Borja Lorente <blorente@apple.com>
2022-03-08 11:08:19 +0000

    WIP

--- go/main.go
+++ go/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"github.com/blorente/standalone-repros/go/lib"
 	"github.com/blorente/standalone-repros/proto"
 	"google.golang.org/grpc"
+	"os"
 )
 
 func main() {
 	lib.MyFun()
+	fmt.Println("FOO:", os.Getenv("FOO"))
 }
 
 func serv(grpcSrv *grpc.Server, protoSrv *proto.FooServiceServer) {}
